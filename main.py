579a08bf0d31a370cac04bbf8147acec23e9f53a
miciukas69x <michailinasmatas@gmail.com>
2025-04-16 16:35:10 +0200

    removed hard-codings, imported sys module to use it with path_to_file, the report is structured

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import number_of_words, characters_count, sorted_list
 
 def get_book_text(path_to_file):
@@ -6,7 +7,11 @@ def get_book_text(path_to_file):
     return file_text
 
 def main():
-    path_to_file = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+        
+    path_to_file = sys.argv[1]
     text = get_book_text(path_to_file)
     num_words = number_of_words(text)
     print (f"Found {num_words} total words.")
